407ae26f79b7b81d5a62f7d394fdbc2b90a4fe04
Kilo Loco <kyle@kiloloco.com>
2024-01-31 21:17:33 -0800

    Initial

---
+++ example-auth/example-auth/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  example-auth
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ example-auth/example-auth/example_authApp.swift
@@ -0,0 +1,16 @@
+//
+//  example_authApp.swift
+//  example-auth
+//
+
+
+import SwiftUI
+
+@main
+struct example_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
